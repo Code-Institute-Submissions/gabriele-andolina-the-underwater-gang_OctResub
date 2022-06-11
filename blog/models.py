0105dcc10b69ad18b09dbd64adbdefdee0083756
Gabriele Andolina <gabe.andolina@gmail.com>
2022-06-11 10:49:57 +0000

    Create Comment model in blog app

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ STATUS = ((0, 'Draft'), (1, 'Published'))
 
 
 class Post(models.Model):
+
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE,
@@ -22,3 +23,19 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"Comment {self.content} by {self.name}"
