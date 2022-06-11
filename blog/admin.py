aee39a5dbfc0639206398a12cf0dd698c4351ff5
Gabriele Andolina <gabe.andolina@gmail.com>
2022-06-11 17:54:10 +0000

    Update PostAdmin class with display, filter and search options

--- blog/admin.py
+++ blog/admin.py
@@ -6,4 +6,8 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
+    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'author', 'created_on')
+    list_filter = ('status', 'created_on')
+    search_fields = ['author', 'title']
     summernote_fields = ('content')
