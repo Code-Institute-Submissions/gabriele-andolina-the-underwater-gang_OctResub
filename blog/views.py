b480d95bd68b7d7bb76c4a228e5e3333be720ff1
Gabriele Andolina <gabe.andolina@gmail.com>
2022-06-17 20:21:01 +0000

    Update PostContent class to render comment_form

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import Post
+from .forms import CommentForm
 
 
 class PostList(generic.ListView):
@@ -17,11 +18,17 @@ class PostContent(View):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.filter(approved=True).order_by('created_on')
+        liked = False
+        if post.likes.filter(username=self.request.user.username).exists():
+            liked = True
 
         return render(
-            request, 
+            request,
             'post_content.html',
             {
                 "post": post,
                 "comments": comments,
-            })
+                "liked": liked,
+                "comment_form": CommentForm()
+            },
+        )
