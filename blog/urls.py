dde0ac4dc53fae8a79ed5e124c52234efbf2f1a7
Gabriele Andolina <gabe.andolina@gmail.com>
2022-06-22 15:19:34 +0000

    Add signin, signout and signup message functionality to base.html template

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>', views.PostContent.as_view(), name="post_content"),
     path('like/<slug:slug>', views.PostLike.as_view(), name="post_like"),
-    path('write/', views.UserPost.as_view(), name='post_create')
+    path('write/', views.UserPost.as_view(), name='post_create'),
 ]
