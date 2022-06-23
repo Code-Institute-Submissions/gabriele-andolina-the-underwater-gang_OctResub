3b655eccaadda5da4d0eed3bdecbf8462ff49e37
Gabriele Andolina <gabe.andolina@gmail.com>
2022-06-23 13:58:18 +0000

    Update Post model with get_absolute_url() and save() methods

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.utils.text import slugify
 from django.urls import reverse
 
 STATUS = ((0, 'Draft'), (1, 'Published'))
@@ -27,6 +28,14 @@ class Post(models.Model):
 
     def likes_tally(self):
         return self.likes.count()
+
+    def get_absolute_url(self):
+        return reverse('post_content', args=[self.slug])
+
+    def save(self, *args):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args)
 
 
 class Comment(models.Model):
