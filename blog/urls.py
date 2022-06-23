ddc4866a402b640f4ed9203dc8806d42ec9dc4d6
Gabriele Andolina <gabe.andolina@gmail.com>
2022-06-23 18:44:23 +0000

    Wire up url for DeletePost view

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('like/<slug:slug>', views.PostLike.as_view(), name="post_like"),
     path('write/', views.UserPost.as_view(), name='post_create'),
     path('update/<slug:slug>', views.UpdatePost.as_view(), name="post_update"),
+    path('delete/<slug:slug>', views.DeletePost.as_view(), name="post_delete"),
     path('<slug:slug>', views.PostContent.as_view(), name="post_content"),
 ]
