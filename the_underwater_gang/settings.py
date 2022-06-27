93faa7e757bea1d0233375ce71f02b7395557f8f
Gabriele Andolina <gabe.andolina@gmail.com>
2022-06-27 22:14:21 +0000

    Fix .gitignore issue

--- the_underwater_gang/settings.py
+++ the_underwater_gang/settings.py
@@ -15,12 +15,11 @@ import os
 import dj_database_url
 from django.contrib.messages import constants as messages
 
-if os.path.isfile("env.py"):
-    import env
+if os.path.isfile("envs.py"):
+    import envs
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
