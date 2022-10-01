5f8d351fac362ffc7b623d7e4ba870480c4924d5
Gabriele Andolina <gabe.andolina@gmail.com>
2022-10-01 17:07:11 +0000

    Wire up url for LogDive view

--- logbook/urls.py
+++ logbook/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('logbook/', views.DiveList.as_view(), name='logbook'),
     path('logbook/<slug:slug>', views.DiveDetails.as_view(), name='dive_details'),
+    path('logbook/log-a-dive/', views.LogDive.as_view(), name='log_dive'),
 ]
