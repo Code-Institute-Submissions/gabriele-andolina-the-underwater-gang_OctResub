c391c9b0ba21879ebd9edd425d4819ec01d44962
Gabriele Andolina <gabe.andolina@gmail.com>
2022-10-02 18:32:33 +0000

    Create url for UpdateDive view

--- logbook/urls.py
+++ logbook/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('logbook/', views.DiveList.as_view(), name='logbook'),
     path('logbook/<slug:slug>', views.DiveDetails.as_view(), name='dive_details'),
     path('logbook/log-a-dive/', views.LogDive.as_view(), name='log_dive'),
+    path('logbook/update/<slug:slug>', views.UpdateDive.as_view(), name='update_dive'),
 ]
