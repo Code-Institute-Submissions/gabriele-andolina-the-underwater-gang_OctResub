d0713eb37c643f3fb4d0750ade404d390bfebb22
Gabriele Andolina <gabe.andolina@gmail.com>
2022-10-06 21:02:15 +0000

    Add 'blog' to urls in blog app to improve UX and consistency with logbook app

--- blog/urls.py
+++ blog/urls.py
@@ -5,9 +5,9 @@ from django.urls import path
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('blog/', views.PostList.as_view(), name='blog'),
-    path('like/<slug:slug>', views.PostLike.as_view(), name="post_like"),
-    path('write/', views.UserPost.as_view(), name='post_create'),
-    path('update/<slug:slug>', views.UpdatePost.as_view(), name="post_update"),
-    path('delete/<slug:slug>', views.DeletePost.as_view(), name="post_delete"),
-    path('<slug:slug>', views.PostContent.as_view(), name="post_content"),
+    path('blog/like/<slug:slug>', views.PostLike.as_view(), name="post_like"),
+    path('blog/write/', views.UserPost.as_view(), name='post_create'),
+    path('blog/update/<slug:slug>', views.UpdatePost.as_view(), name="post_update"),
+    path('blog/delete/<slug:slug>', views.DeletePost.as_view(), name="post_delete"),
+    path('blog/<slug:slug>', views.PostContent.as_view(), name="post_content"),
 ]
